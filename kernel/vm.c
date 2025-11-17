1770dc296c1f17a8e9264695ef37d0cd2ec522bd
baijj <912473292@qq.com>
2025-11-17 15:03:29 +0800

    printf页表

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,34 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+int kama_pgtblorint(pagetable_t pagetable , int depth)
+{
+
+  for (int i =0; i< 512; i++)
+  {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V)
+    {
+      printf("..");
+      for (int j = 0; j < depth; j++)
+      {
+        printf(" ..");
+      }
+      printf("%d: pte:%p  pa:%p\n",i,pte,PTE2PA(pte));
+
+      if ((pte & PTE_V) && (PTE_R|PTE_W|PTE_X)== 0)
+      {
+        uint64 child = PTE2PA(pte);
+        kama_pgtblorint(child , depth+1);
+      }
+    }
+    return 0;
+  }
+}
+
+int kama_vmprint(pagetable_t pagetable)
+{
+  printf("page table %p \n",pagetable);
+  return kama_pgtblorint(pagetable,0);
+}
